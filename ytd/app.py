eace8125a2fdc4a4a1b7364d26f2621974de202c
mitesh2205 <48359593+mitesh2205@users.noreply.github.com>
2020-11-22 20:49:54 +0530

    Add files via upload

--- ytd/app.py
+++ ytd/app.py
@@ -1,22 +1,32 @@
 from flask import Flask , url_for , redirect , render_template , request , flash
 import pytube
+from flask import send_file
 import os
 app = Flask(__name__)
 app.secret_key = "mitesh"
+# global title
 @app.route('/')
 def index():
     return render_template("index.html")
 
 @app.route('/download',methods=["POST" , "GET"])
 def download():
-    SAVE_PATH = 'C:' + '/BMADE-Youtube Video'
+    SAVE_PATH = 'BMADE-Youtube Video/'
     url = request.form["yt_url"]
     youtube = pytube.YouTube(url)
+    global title
+    title = youtube.title
     video = youtube.streams.first()
     video.download(SAVE_PATH)
-    flash("Your video is downloaded successfully at "+ SAVE_PATH +", Enter other link below to download video.")
+    flash("Video is available, Enter other link to download other video.")
     # flash(SAVE_PATH)
-    return render_template("index.html")
+    return render_template("index.html", path = SAVE_PATH + title+".mp4" , title = title+".mp4")
+
+@app.route('/downloadfile')
+def downloadFile ():
+
+    paths = "BMADE-Youtube Video/"
+    return send_file(os.path.join(paths, title +".mp4"), as_attachment=True)
 
 if __name__ == '__main__':
     app.run( debug = True )
